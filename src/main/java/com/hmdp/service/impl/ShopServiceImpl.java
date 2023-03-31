b526f85b3f2b757a4acbb625624a0d2f1b19fc25
jtzhao <zhao0123>
2023-03-31 15:10:10 +0800

    利用互斥锁解决缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hmdp.service.impl;
 
+import ch.qos.logback.core.util.TimeUtil;
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -16,8 +18,7 @@ import javax.annotation.Resource;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -36,25 +37,128 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        //防止缓存穿透
+//        Shop shop = queryWithPassThrough(id);
+
+        //互斥锁解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if(shop==null){
+            return Result.fail("店铺不存在!");
+        }
+        return Result.ok(shop);
+    }
+
+
+
+    /**
+     * 防止缓存穿透
+     * @param id
+     * @return
+     */
+    public Shop queryWithMutex(Long id)  {
+        String lockKey=LOCK_SHOP_KEY+id;
+        Shop shop=null;
         //1、从redis查询商铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
         //2、判断是否存在
         if(StrUtil.isNotBlank(shopJson)){
             //3、存在，直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        //未命中且命中空值
+        if("".equals(shopJson)){
+            return null;
+        }
+
+        try {
+            //实现缓存重建
+            //获取互斥锁
+            //判断是否获取成功
+            while(true){
+                boolean isLock = tryLock(lockKey);
+                if(!isLock){
+                    //失败则休眠并重试
+                    Thread.sleep(50);
+                    continue;
+                }else{
+                    break;
+                }
+            }
+
+            //成功，根据id查询数据库
+            //4、不存在，根据id查询数据库
+            shop = getById(id);
+            //模拟重建延时
+            Thread.sleep(200);
+
+            //5、不存在，返回错误
+            if(shop==null){
+                //返回空值，防止缓存穿透
+                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+            //6、存在，写入redis
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //释放互斥锁
+            unlock(lockKey);
+        }
+        return shop;
+    }
+
+
+
+
+
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+
+    /**
+     * 防止缓存穿透
+     * @param id
+     * @return
+     */
+    public Shop queryWithPassThrough(Long id){
+        //1、从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        //2、判断是否存在
+        if(StrUtil.isNotBlank(shopJson)){
+            //3、存在，直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        if("".equals(shopJson)){
+            return null;
         }
         //4、不存在，根据id查询数据库
         Shop shop = getById(id);
         //5、不存在，返回错误
         if(shop==null){
-            return Result.fail("店铺不存在！");
+            //返回空值，防止缓存穿透
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
         }
         //6、存在，写入redis
         stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
-
-        return Result.ok(shop);
+        return shop;
     }
+
+
+
+
+
+
+
+
+
 
     @Override
     @Transactional
